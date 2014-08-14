38988b917802238db5ca65021f62ce2ea819cd08
Michael Liao <askxuefeng@gmail.com>
2014-08-14 21:07:33 +0800

    add gmail example

---
+++ email/sendmail_tls.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+from_addr = raw_input('Gmail: ')
+password = raw_input('Password: ')
+to_addr = raw_input('To: ')
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
