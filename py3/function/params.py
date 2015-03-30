1aac93f32cddc4f6f575b6fd48cf8ea91c1bab63
Michael Liao <askxuefeng@gmail.com>
2015-03-30 22:21:36 +0800

    add function examples

---
+++ py3/function/params.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def power(x, n=2):
+    return x ** n
+
+print('power(5) =', power(5))
+print('power(5, 2) =', power(5, 2))
+print('power(5, 4) =', power(5, 4))
