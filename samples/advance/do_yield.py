a52ba6fd5b0d799e4fb9bfcd9a67cd40493cd215
Michael Liao <askxuefeng@gmail.com>
2015-05-02 15:45:39 +0800

    update yield sample

--- samples/advance/do_yield.py
+++ samples/advance/do_yield.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import random, math
+def each_ascii(s):
+    for ch in s:
+        yield ord(ch)
+    return '%s chars' % len(s)
 
-def rnd(count):
-    while count > 0:
-        count = count - 1
-        r = random.randint(0, 100)
-        print('-- random number --')
-        # 返回随机数r:
-        yield r
-        # 代理返回tri(r)的yield，可能有很多次:
-        ok = yield from tri(r)
-        # 相当于:
-        # for x in tri(r):
-        #     yield x
-        print('-- %s --' % ok)
-    return 'done'
-
-def tri(r):
-    yield r * r
-    yield r * r * r
-    # 相当于: raise StopIteration('ok')
-    return 'ok'
+def yield_from(g):
+    try:
+        while True:
+            next(g)
+    except StopIteration as s:
+        return s.value
 
 def main():
-    for x in rnd(3):
-        print(x)
+    for x in each_ascii('abc'):
+        print(x) # => 'a', 'b', 'c'
+    it = each_ascii('xyz')
+    try:
+        while True:
+            print(next(it)) # => 'x', 'y', 'z'
+    except StopIteration as s:
+        print(s.value) # => '3 chars'
+
+    # using yield from in main() will change main() from function to generator:
+    # r = yield from each_ascii('hello')
+
+    print(yield_from(each_ascii('hello'))) # => '5 chars'
 
 main()
